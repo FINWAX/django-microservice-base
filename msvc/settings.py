050cc6c8c40dcded8073f61be9d211dc60e77cc9
Pavel E. <deusagnis@gmail.com>
2025-03-15 13:34:16 +0300

    Update settings.

--- msvc/settings.py
+++ msvc/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = settings.get("DJANGO_SECRET_KEY", "127.0.0.1")
+SECRET_KEY = settings.get("APP_SECRET_KEY", "127.0.0.1")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(settings.get("DEBUG_MODE", default=0))
