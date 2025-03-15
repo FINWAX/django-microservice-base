7ad595003779ebdc9b3045dbb63fff5a32534462
Pavel E. <deusagnis@gmail.com>
2025-03-15 13:35:29 +0300

    Update example routes.

--- app/views.py
+++ app/views.py
@@ -1,18 +1,18 @@
 from authlib.integrations.django_oauth2 import ResourceProtector
 from django.contrib.auth.models import User
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest
 from rest_framework import permissions, viewsets
 
 from app.auth.basic_validator import BasicZitadelIntrospectTokenValidator
-from app.auth.jwt_validator import JWTZitadelIntrospectTokenValidator
 from app.models import Plug
 from app.serializers import UserSerializer, PlugSerializer
 
 require_basic_auth = ResourceProtector()
 require_basic_auth.register_token_validator(BasicZitadelIntrospectTokenValidator())
 
-require_jwt_auth = ResourceProtector()
-require_jwt_auth.register_token_validator(JWTZitadelIntrospectTokenValidator())
+
+# require_jwt_auth = ResourceProtector()
+# require_jwt_auth.register_token_validator(JWTZitadelIntrospectTokenValidator())
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -26,21 +26,55 @@ class PlugViewSet(viewsets.ModelViewSet):
     serializer_class = PlugSerializer
 
 
+# Example of endpoint without user authentication.
+def unprotected_hello(request: HttpRequest):
+    output = {
+        'ok': True,
+        'message': f'Hello {request.GET.get('name', 'stranger')}!',
+        'token': getattr(request, 'oauth_token', None)
+    }
+
+    return JsonResponse(output)
+
+
 # Example of endpoint authenticated by basic auth.
 @require_basic_auth()
-def custom_view_basic(request):
+def protected_hello(request: HttpRequest):
     output = {
-        'token': request.oauth_token
+        'ok': True,
+        'message': f'Hello {request.GET.get('name', 'stranger')}!',
+        'token': getattr(request, 'oauth_token', None)
     }
 
     return JsonResponse(output)
 
+# # Example of endpoint authenticated by JWT auth.
+# @require_jwt_auth()
+# def protected_hello(request: HttpRequest):
+#     output = {
+#         'message': f'Hello {request.GET.get('name', 'stranger')}!',
+#         'token': getattr(request, 'oauth_token', None)
+#     }
+#
+#     return JsonResponse(output)
 
-# Example of endpoint authenticated by JWT auth.
-@require_jwt_auth()
-def custom_view_jwt(request):
-    output = {
-        'token': request.oauth_token
-    }
+def health_check(request: HttpRequest):
+    """
+    Check that service is healthy.
+    :param request: Запрос.
+    :return: Ответ.
+    """
+    return JsonResponse({
+        'ok': True,
+    })
 
-    return JsonResponse(output)
+def health_availability(request: HttpRequest):
+    """
+    Check that service is available.
+    :param request: Запрос.
+    :return: Ответ.
+    """
+    return JsonResponse({
+        'ok': True,
+    })
+
--- msvc/urls.py
+++ msvc/urls.py
@@ -2,15 +2,17 @@ from django.urls import include, path
 from rest_framework import routers
 
 from app import views
-from app.views import custom_view_basic, custom_view_jwt
+from app.views import unprotected_hello, protected_hello, health_availability, health_check
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'plugs', views.PlugViewSet)
 
 urlpatterns = [
-    path('custom_basic', custom_view_basic),
-    path('custom_jwt', custom_view_jwt),
+    path('health/check', health_check),
+    path('health/availability', health_availability),
+    path('greeting/hello-unprotected', unprotected_hello),
+    path('greeting/hello-protected', protected_hello),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
